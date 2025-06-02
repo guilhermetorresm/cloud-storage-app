90e3ebd1d9fff0f84ee44520aa2ae7a08fbd94d0
Jordana <jordanafranca@ufpi.edu.br>
2025-06-02 00:05:42 -0300

    Alteração na tela de cadastro

--- frontend/src/Pages/register.jsx
+++ frontend/src/Pages/register.jsx
@@ -4,31 +4,24 @@ import { Button } from "../Components/ui/button";
 import TopbarNoSearch from "../Components/TopbarNoSearch";
 
 export default function Register() {
+  const [nome, setNome] = useState("");
+  const [usuario, setUsuario] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
 
-const[nome,setNome] = useState("");
-const [usuario, setUsuario] = useState("");
-const [email, setEmail] = useState("");
-const [senha, setSenha] = useState("");
-const [confirmarSenha, setConfirmarSenha] = useState("");
-
-
-const handleCadastro = () => {
-    console.log("Cadastro:", { nome, usuario,email, senha, confirmarSenha });
+  const handleCadastro = () => {
+    console.log("Cadastro:", { nome, usuario, email, senha, confirmarSenha });
   };
 
-
-
-
-
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex flex-col min-h-screen">
       <TopbarNoSearch />
-      <div className="flex h-screen">
-        <main className="flex-1 bg-gray-100 ">
-          <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+      
+        <main className="flex-1 bg-gray-100 flex items-center justify-center p-4">
             <div className="bg-white rounded-3xl shadow-lg p-8 w-full max-w-md">
+              <h1 className="text-xl font-bold text-center  mb-5"> Cadastre-se </h1>
               <div className="space-y-4">
-
                 <InputField
                   type="text"
                   placeholder="Nome"
@@ -49,7 +42,7 @@ const handleCadastro = () => {
                   type="text"
                   placeholder="Email"
                   icon="email"
-                  value={usuario}
+                  value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
 
@@ -69,17 +62,14 @@ const handleCadastro = () => {
                   onChange={(e) => setConfirmarSenha(e.target.value)}
                 />
 
-
                 <Button onClick={handleCadastro} type="submit">
                   Finalizar Cadastro
                 </Button>
-
-                
               </div>
             </div>
-          </div>
+
         </main>
-      </div>
+    
     </div>
   );
 }
