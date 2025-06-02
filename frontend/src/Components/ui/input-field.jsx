a92c740d0e67c4369ad5beac36042d65af8947ca
Jordana <jordanafranca@ufpi.edu.br>
2025-06-01 23:37:04 -0300

    Adição dos input-field e button na tela de cadastro

--- frontend/src/Components/ui/input-field.jsx
+++ frontend/src/Components/ui/input-field.jsx
@@ -1,10 +1,14 @@
 "use client";
 
 import React from "react";
-import { User, Lock } from "lucide-react";
+import { User, Lock, Mail } from "lucide-react";
 
 export function InputField({ type, placeholder, icon, value, onChange }) {
-  const IconComponent = icon === "user" ? User : Lock;
+  const IconComponent =
+  icon === "user" ? User :
+  icon === "lock" ? Lock :
+  icon === "email" ? Mail :
+  null;
 
   return (
     <div className="relative">
--- frontend/src/Pages/register.jsx
+++ frontend/src/Pages/register.jsx
@@ -1,11 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputField } from "../Components/ui/input-field";
+import { Button } from "../Components/ui/button";
 import TopbarNoSearch from "../Components/TopbarNoSearch";
-export default function Register(){
-    return(
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
-            <TopbarNoSearch/>
 
-        </div>
+export default function Register() {
 
-    );
+const[nome,setNome] = useState("");
+const [usuario, setUsuario] = useState("");
+const [email, setEmail] = useState("");
+const [senha, setSenha] = useState("");
+const [confirmarSenha, setConfirmarSenha] = useState("");
+
+
+const handleCadastro = () => {
+    console.log("Cadastro:", { nome, usuario,email, senha, confirmarSenha });
+  };
+
+
+
+
+
+  return (
+    <div className="flex h-screen flex-col">
+      <TopbarNoSearch />
+      <div className="flex h-screen">
+        <main className="flex-1 bg-gray-100 ">
+          <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+            <div className="bg-white rounded-3xl shadow-lg p-8 w-full max-w-md">
+              <div className="space-y-4">
+
+                <InputField
+                  type="text"
+                  placeholder="Nome"
+                  icon="user"
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                />
+
+                <InputField
+                  type="text"
+                  placeholder="Usuário"
+                  icon="user"
+                  value={usuario}
+                  onChange={(e) => setUsuario(e.target.value)}
+                />
+
+                <InputField
+                  type="text"
+                  placeholder="Email"
+                  icon="email"
+                  value={usuario}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <InputField
+                  type="password"
+                  placeholder="Senha"
+                  icon="lock"
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                />
+
+                <InputField
+                  type="password"
+                  placeholder="Confirmar senha"
+                  icon="lock"
+                  value={confirmarSenha}
+                  onChange={(e) => setConfirmarSenha(e.target.value)}
+                />
+
+
+                <Button onClick={handleCadastro} type="submit">
+                  Finalizar Cadastro
+                </Button>
+
+                
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
