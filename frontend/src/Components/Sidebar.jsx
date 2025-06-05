605abdf3527becf2919788cf7d0f0163e5f1af7c
Naparanagua <nivaldoparanagua2001@hotmail.com>
2025-06-05 20:48:21 -0300

    Ajustar altura da sidebar e garantir que o botão Sair fique fixo na base

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -11,7 +11,7 @@ export default function Sidebar() {
   };
 
   return (
-    <aside className="w-60 h-6/6 bg-white border-r shadow-sm flex flex-col justify-between p-4 overflow-hidden">
+    <aside className="w-60 h-full bg-white border-r shadow-sm flex flex-col p-4 overflow-hidden">
       {/* Conteúdo principal */}
       <div>
         <nav className="space-y-4">
@@ -27,13 +27,13 @@ export default function Sidebar() {
       </div>
 
       {/* Botão "Sair" fixo na base da sidebar */}
-      <div className="pt-4 border-t">
-        <button 
+      <div className="pt-4 border-t mt-auto">
+      <button 
         onClick={handleExit} 
-        className="z-10 text-red-600 font-semibold flex items-center space-x-2 hover:underline w-full">
-          <LogOut/>
-          <span>Sair</span>
-        </button>
+        className="mt-auto z-10 text-red-600 font-semibold flex items-center space-x-2 hover:underline w-full">
+        <LogOut/>
+        <span>Sair</span>
+      </button>
       </div>
     </aside>
   );
