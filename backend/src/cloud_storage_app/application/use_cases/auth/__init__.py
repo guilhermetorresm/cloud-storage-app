6c5da2b61f71b177a1f160185d2e5522f9858f6d
Guilherme Torres Melo <guilhermetorresm77@gmail.com>
2025-06-07 21:13:42 -0300

    feat: Adiciona o caso de uso GetCurrentUser para obter dados do usuário autenticado, incluindo validação de token JWT e tratamento de erros. Atualiza os DTOs e a estrutura de importação nos módulos de autenticação e usuário.

--- backend/src/cloud_storage_app/application/use_cases/auth/__init__.py
+++ backend/src/cloud_storage_app/application/use_cases/auth/__init__.py
@@ -1,8 +1,5 @@
 from .login_use_case import LoginUseCase
-from .get_current_user_use_case import GetCurrentUserUseCase, GetCurrentUserRequest
 
 __all__ = [
     "LoginUseCase",
-    "GetCurrentUserUseCase",
-    "GetCurrentUserRequest"
 ]
--- backend/src/cloud_storage_app/application/use_cases/user/__init__.py
+++ backend/src/cloud_storage_app/application/use_cases/user/__init__.py
@@ -3,6 +3,10 @@ Módulo de casos de uso de autenticação.
 """
 
 from cloud_storage_app.application.use_cases.user.create_user_use_case import CreateUserUseCase
+from cloud_storage_app.application.use_cases.user.get_current_user_use_case import GetCurrentUserUseCase
+
+
 __all__ = [
     "CreateUserUseCase",
+    "GetCurrentUserUseCase",
 ]
