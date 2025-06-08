597bbe01e9813e96b12770eea708501bab44ad19
Naparanagua <nivaldoparanagua2001@hotmail.com>
2025-06-08 15:21:25 -0300

    adiciona suporte a cookies na configuração da API

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // 'http://localhost:8080'
-  withCredentials: true // caso use cookies (JWT via cookie, por exemplo)
 });
 
 export default api;
