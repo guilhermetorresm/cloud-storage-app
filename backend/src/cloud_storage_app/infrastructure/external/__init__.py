245706f52534f5cec2e5ebc659e175ed9b83f02f
WhityWolf <felipehac@hotmail.com>
2025-07-06 16:17:12 -0300

    feat: audio_processing_interface.py e audio_processing_service.py, #63.

--- backend/src/cloud_storage_app/infrastructure/external/__init__.py
+++ backend/src/cloud_storage_app/infrastructure/external/__init__.py
@@ -3,8 +3,10 @@ External services implementations
 """
 from .image_processing_service import PillowImageProcessingService
 from .thumbnail_generator_service import PillowThumbnailGeneratorService
+from .audio_processing_service import MutagenAudioProcessingService
 
 __all__ = [
     'PillowImageProcessingService',
-    'PillowThumbnailGeneratorService'
+    'PillowThumbnailGeneratorService',
+    'MutagenAudioProcessingService'
 ]
